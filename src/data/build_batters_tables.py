6f477e2e99acc8d867436d4d042dde24953593b9
Sam L <lafell.samuel@gmail.com>
2023-01-10 18:25:35 -0500

    building batter table

--- src/data/build_batters_tables.py
+++ src/data/build_batters_tables.py
@@ -5,6 +5,8 @@ Purpose: To have a database locally to pull from and be able to quickly scan for
 
 """
 
+
+
 # Working with PostgreSQL
 # Installing in Python: hhttps://www.psycopg.org/psycopg3/docs/basic/install.html
 import psycopg
@@ -16,7 +18,7 @@ import pandas as pd
 # Set base path
 try:
     base_path = pathlib.Path(__file__).absolute().parents[2]
-except:
+except Exception:
     base_path = pathlib.Path.cwd().absolute()
 
 # Add utils folder if not already in system path
@@ -47,14 +49,61 @@ schema, table = 'players', 'chadwick_register'
 try:
     cur.execute(f"SELECT * FROM {schema}.{table}")
     data_fromtable = cur.fetchall()
-    data = pd.DataFrame(data_fromtable, columns = sf.get_table_col_names(connection, schema+'.'+table))
+    data = pd.DataFrame(
+        data_fromtable,
+        columns=sf.get_table_col_names(connection, f'{schema}.{table}'),
+    )
     print(f'successfully loaded {schema}.{table} with {data.shape[0]} rows')
 except psycopg.Error as e:
     print(e)
-    
+
 # * For a given year, calculcate the xBA for a batter for each game and load to a table
 # -------------------------------------------------------
 # get all statcast data for July 4th, 2017
 from pybaseball import statcast
-july4_statcast = statcast('2017-07-04')
-july4_statcast
+month_statcast = statcast(start_dt='2017-07-04', end_dt='2017-08-04')
+
+# Get ID, year, and Team
+from pybaseball.lahman import *
+
+
+# At bats
+def get_atbats(df):
+    atbats = df.groupby(['game_date', 'batter', 'at_bat_number']).agg({'batter':'nunique'})
+    atbats.columns = ['at_bats']
+    atbats = atbats.reset_index()
+    atbats = atbats.drop(columns=['at_bat_number'], axis=1).groupby(['game_date', 'batter']).sum(numeric_only=True).reset_index()
+    return atbats
+
+# Estimated Batting Average
+def get_batter_eba(df):
+    return df.groupby(['game_date', 'batter']).agg({'estimated_ba_using_speedangle':'median'}).reset_index().fillna(0)
+
+# Get Player ID, year, and team
+def get_player_year_team(df):
+    game_year = df.game_date.dt.year.unique()[0]
+    batting = pybaseball.lahman.batting()
+    batting = batting.loc[batting['yearID'] == game_year]
+    return batting[['playerID', 'yearID', 'teamID']]
+
+batter_info = get_atbats(month_statcast).merge(get_batter_eba(month_statcast), on=['game_date', 'batter'], how='inner')
+#batter_info.merge(data, how = 'inner', left_on='batter', right_on='key_mlbam')
+
+# Join Fangraphs ID to Team
+data_short = data[['key_bbref', 'key_mlbam']]
+batting_team_info = get_player_year_team(month_statcast).merge(data_short, how='inner', left_on='playerID', right_on='key_bbref').drop(['key_bbref', 'playerID', 'yearID'], axis = 1)
+
+# Finally, get date, batter mlbam, and teamID
+batting_info = batter_info.merge(batting_team_info, how='inner', left_on='batter', right_on='key_mlbam').drop('key_mlbam', axis = 1)
+batting_info.batter.unique()[0]
+
+
+# get all of this season's batting data so far
+import pybaseball
+pybaseball.teams()
+
+# People traded in the middle of the season testing
+data.loc[data['name_last'] == 'Cruz'] # person traded in the middle of the season
+batting_stats = batting_stats(2017, qual = 5)
+batting_stats.loc[batting_stats['IDfg'] == 21711] # Bating stats
+batting_info.loc[batting_info['batter'] == 665833]
